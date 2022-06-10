198b3dac898ea879ca7405250f25c83fb3ec26a7
Wallace Oliveira <104787895+Wallace-Salazar@users.noreply.github.com>
2022-06-10 15:20:58 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,61 @@
+/* console.log (3 > 2) // true
+console.log (2 > 3) // false */
+
+/*--------operação de comparação-----------*/
+
+// = atribuição de tipo
+// == verificação valor
+// === verificação de valor e tipo
+// != verifica se os valores comparados são diferentes
+// !== verifica se os valores e tipos são diferentes
+// > maior que
+// < menor que
+// >= maior ou igual que
+// <= menor ou igual que
+
+/*    let dado = 25
+      let number = "255"
+      console.log( dado < number)   */
+
+/* ------------estruturas condicionais------------ */
+
+// if = se
+// else = se não
+
+/*       let dado = 18
+         if (dado >= 18){
+         console.log('voce pode acessar meu site')
+         }else{
+         console.log('você não pode passar')
+         }       */
+
+/* --------------operadores logicos-------------- */
+
+// && (and) verifica se amlos os valores são verdadeiros
+// || (or) verifica se um dos valores é verdadeiro
+// ! (not) inverte o valor que é dado à ele
+
+/*      console.log(true && false) // false
+        console.log(true && true) // true
+        console.log(false || true) //true
+        console.log(false || false) //false      
+
+          let a = 3 > 2
+          let b = 2 > 3
+          if(a == true && b == false){
+          console.log('as duas condições são verdadeiras')
+          } else {
+          console.log('as duas condições são falsas')
+          }    */
+
+/*
+          let dado = true
+          console.log(!dado)   */
+
+/*        let mes = "julho";
+
+          if (mes == "dezembro" || mes == "janeiro") {
+          console.log("Parabens");
+          } else {
+          console.log("não é seu aniversario");}
+*/
